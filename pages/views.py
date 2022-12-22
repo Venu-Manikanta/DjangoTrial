0c07380e9707cada9439e0aac3cdfefe6c475c5f
Venu-Manikanta <107348567+VenuManikantaUA@users.noreply.github.com>
2022-12-22 16:25:40 +0530

    YO

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from PIL import Image
 
 # Create your views here.
 def homePageView(request):
-    return HttpResponse("Hey, My app is running")
+    try:
+        with open("app/image.jpeg", "rb") as f:
+            return HttpResponse(f.read(), content_type="image/jpeg")
+    except IOError:
+        red = Image.new('RGBA', (1, 1), (255,0,0,0))
+        response = HttpResponse(content_type="image/jpeg")
+        red.save(response, "JPEG")
+        return response
